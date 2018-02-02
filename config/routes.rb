166fb3bf9c864ac6589a714a743982f6e4b7f7a8
Marat Galiev <kazanlug@gmail.com>
2018-02-02 15:26:53 +0300

    Change upload path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
-  #get '/uploads/grid/shop/preview_image/:id/:filename', to: 'gridfs#preview_image', as: 'grid_image'
-  #get "/shops/:id/preview_image" => "gridfs#serve", as: 'grid_image'
-  get '/uploads/grid/shop/preview_image/:id/:filename' => 'gridfs#preview_image', as: 'grid_image'
+  get '/files/grid/shop/preview_image/:id/:filename' => 'gridfs#preview_image', as: 'grid_image'
 end
